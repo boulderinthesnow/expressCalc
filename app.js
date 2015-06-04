499ad2f6871d955b8edee7b61118d339180b82ef
Matthew Williams <mwilliamsnov6@gmail.com>
2015-06-03 17:46:17 -0700

    refactored

--- app.js
+++ app.js
@@ -24,26 +24,51 @@ app.set("view engine", "ejs")
 // 	console.log("Starting a server on localhost:3000");
 // })
 
-app.get("/add/:num1/:num2", function(req, res) {
-	var sum = Number(req.params.num1) + Number(req.params.num2)
-	res.render("math", {number:sum});
-});
 
-app.get("/mult/:num1/:num2", function(req, res) {
-	var sum = Number(req.params.num1) * Number(req.params.num2)
-	res.render("math", {number:sum});
-});
 
-app.get("/sub/:num1/:num2", function(req, res) {
-	var sum = Number(req.params.num1) - Number(req.params.num2)
-	res.render("math", {number:sum});
-});
+// app.get("/add/:num1/:num2", function(req, res) {
+// 	var sum = Number(req.params.num1) + Number(req.params.num2)
+// 	res.render("math", {number:sum});
+// });
 
-app.get("/div/:num1/:num2", function(req, res) {
-	var sum = Number(req.params.num1) / Number(req.params.num2)
-	res.render("math", {number:sum});
-});
+// app.get("/mult/:num1/:num2", function(req, res) {
+// 	var sum = Number(req.params.num1) * Number(req.params.num2)
+// 	res.render("math", {number:sum});
+// });
 
+// app.get("/sub/:num1/:num2", function(req, res) {
+// 	var sum = Number(req.params.num1) - Number(req.params.num2)
+// 	res.render("math", {number:sum});
+// });
+
+// app.get("/div/:num1/:num2", function(req, res) {
+// 	var sum = Number(req.params.num1) / Number(req.params.num2)
+// 	res.render("math", {number:sum});
+// });
+//////////////////////////
+
+
+app.get("/:whatever/:num1/:num2", function(req, res) {
+	sum = Number(req.params.num1) + Number(req.params.num2)
+	sub = Number(req.params.num1) - Number(req.params.num2)
+	div = Number(req.params.num1) / Number(req.params.num2)
+	mult = Number(req.params.num1) * Number(req.params.num2)
+	whatever = req.params.whatever
+
+	if (whatever === "add") {
+		res.render("math", {number:sum});
+	}
+	if (whatever === "sub") {
+		res.render("math", {number:sub});
+	}
+	if (whatever === "div") {
+		res.render("math", {number:div});
+	}
+	if (whatever === "mult") {
+		res.render("math", {number:mult});
+	}
+	
+});
 
 
 app.listen(3000, function(){
